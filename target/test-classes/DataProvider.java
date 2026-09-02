91619cf6d4930604d61ae0d7ad5c38aba89214fb
Prashant Kumar <pk19960708@gmail.com>
2026-09-02 19:36:59 +0530

    Removed obsolete files

---
+++ target/test-classes/DataProvider.java
@@ -0,0 +1,16 @@
+
+public class DataProvider {
+
+	public Object[][] DataProvider(name = "loginCRM") {
+		// TODO Auto-generated constructor stub
+		
+		
+		return new Object[][] {
+			
+			{"incorrect@gmail.com","paswrd"},
+			{"pk19960708@gmail.com", "TUByYXRob24xMjM0"}
+			
+		};
+	}
+
+}
